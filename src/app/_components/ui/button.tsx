62256ce8424072c4de99cab5291d8d76587540ce
Icaro <icarogomesmotta@gmail.com>
2024-06-04 16:40:01 -0300

    fix: resolvendo erro do isLoading

--- src/app/_components/ui/button.tsx
+++ src/app/_components/ui/button.tsx
@@ -38,11 +38,14 @@ export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean;
-  isLoading?: boolean;
+  isLoading: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  (
+    { className, variant, size, asChild = false, isLoading = false, ...props },
+    ref,
+  ) => {
     const Comp = asChild ? Slot : 'button';
 
     return (
@@ -51,7 +54,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         ref={ref}
         {...props}
       >
-        {props.children} {props.isLoading && <Spinner />}
+        {props.children} {isLoading && <Spinner />}
       </Comp>
     );
   },
